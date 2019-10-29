04e9d97b36dea63f3e64cf54390e9ddfd8f4a590
KatherinMaria04 <katherinmaria04@gmail.com>
2019-10-29 15:40:26 +0530

    Modified Cors

--- src/main/java/com/revature/charityappcontentms/config/CorsConfig.java
+++ src/main/java/com/revature/charityappcontentms/config/CorsConfig.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig  implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080")
+        String origins = "http://localhost:4200";
+		registry.addMapping("/**").allowedOrigins(origins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD").allowCredentials(true);
     }
 
